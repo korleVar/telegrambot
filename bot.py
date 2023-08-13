d61396d2e41da1bee05c499d2e515e2769aef03e
Kororoleva Varvara <isip_v.v.koroleva@mpt.ru>
2023-08-13 13:43:32 +0300

    13.43

--- bot.py
+++ bot.py
@@ -1,3 +1,10 @@
+import pymongo
+
+client = pymongo.MongoClient('mongodb://localhost:27017/')
+db = client['TelegramBotDb']
+collection = db['users']
+
+
 
 # импорт библиотеки
 import telebot
@@ -11,29 +18,46 @@ from telebot import types
 
 
 # реагирует сообщение
+# @botTimeWeb.message_handler(content_types=['text'])
 @botTimeWeb.message_handler(commands=['start'])
 
-# объявление функции startBot, которая принимает сообщение от пользователя
-def startBot(message):
 
-    #переменной присвоенно сообщение с именем и фамилией пользователя
-    first_mess = f"<b>{message.from_user.first_name} {message.from_user.last_name}</b>, привет!\nХочешь расскажу немного о тебе?"
-    
+# объявление функции startBot, которая принимает сообщение от пользователя
+def text(message):
+
+    #переменной присвоенно сообщение 
+    user_mess=message.text
+
+    # вставка данныхых в колекцию
+    collection.insert_one({"message": user_mess})
+
     #интерактивная клава
     markup = types.InlineKeyboardMarkup()
     
     # кнопка ДА
     button_yes = types.InlineKeyboardButton(text='Да', callback_data='yes')
-    button_no = types.InlineKeyboardButton(text='Нет', callback_data='no')
     
     #для отображение как клавиутура в боте
-    markup.add(button_yes, button_no)
+    markup.add(button_yes)
 
     # отправляет сообщение конкретному пользователю пользователю
-    botTimeWeb.send_message(message.chat.id, first_mess, parse_mode='html', reply_markup=markup)
+    botTimeWeb.send_message(message.chat.id, user_mess,  parse_mode='html', reply_markup=markup)
 
 
- 
+
+
+
+# реагирует сообщение
+@botTimeWeb.message_handler(commands=['all'])
+
+def send_card(message):
+       # Получение всех документов из коллекции
+    all_documents = collection.find()
+    # Вывод всех документов
+    for document in all_documents:
+       if 'message' in document:
+        print(document['message'])
+
 
 # включает режим ожидания новых сообщений
 botTimeWeb.infinity_polling()
